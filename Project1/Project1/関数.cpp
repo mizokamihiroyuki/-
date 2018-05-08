1f5b86beb08721cb92f19034dfef6ce40bda43c3
student <student@DEV2-05>
2018-05-08 12:07:05 +0900

    a

---
+++ Project1/Project1/関数.cpp
@@ -0,0 +1,64 @@
+/*関数
+関数は1つの仕事だけをする。
+関数は引数と言う
+与えられた値を元に仕事を実施し、返り値で結果を返す。
+
+int sub(int x)
+{
+「
+l処理(仕事内容：Xを元に処理)
+l
+l
+{
+return 返り値；
+
+例
+int sub(int x)			}
+{						l
+int y=0;		}		l
+y=x*x;		l処理	l関数
+y++;			}		l
+return y				l
+}						}
+void main()
+{
+int x=2;
+int a=0;
+a=sub(x);
+printf("%d",a);
+a=sub(3);
+printf("%d",a);
+}
+
+値が違うだけのアルゴリズムであれば同じ関数で処理させる
+
+float sub(int x,int y)
+{
+int z=0;
+for(int i=0;i<y;i++)
+z+=x;
+
+return z;
+}
+void main()
+{
+int a=0;
+a=sub(2,3)+sub(4,3);
+}
+}*/
+
+//問4　入力した値が図の中であればOK
+//	   そうでなければNOと出力しなさい。
+
+int sub(int x)
+{
+	int a = 0;
+}
+
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+
+
+}
